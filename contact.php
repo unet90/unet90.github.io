f892f2713e739b552878db1019e352b02ef10b76
Vershinin Vladislav <unet90@users.noreply.github.com>
2017-10-22 20:22:03 +0300

    commit

--- contact.php
+++ contact.php
@@ -1,42 +1,61 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
+
 $method = $_SERVER['REQUEST_METHOD'];
-//Script Foreach
-$c = true;
-if ( $method === 'POST' ) {
-	$name = trim($_POST["name"]);
-	$mail = trim($_POST["mail"]);
-	$comment = trim($_POST["comment"]);
-	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "name" && $key != "mail" && $key != "comment" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
-} else if ( $method === 'GET' ) {
-	$name = trim($_GET["name"]);
-	$mail = trim($_GET["mail"]);
-	$comment = trim($_GET["comment"]);
-	foreach ( $_GET as $key => $value ) {
-		if ( $value != "" && $key != "name" && $key != "mail" && $key != "comment" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
+/* Осуществляем проверку вводимых данных и их защиту от враждебных 
+скриптов */
+$name = htmlspecialchars($_POST["name"]);
+$mail = htmlspecialchars($_POST["mail"]);
+$comment = htmlspecialchars($_POST["comment"]);
+/* Устанавливаем e-mail адресата */
+$myemail = "vladi4ek1990@mail.ru";
+/* Проверяем заполнены ли обязательные поля ввода, используя check_input 
+функцию */
+$name = check_input($_POST["name"], "Введите ваше имя!");
+$mail = check_input($_POST["mail"], "Введите ваш e-mail!");
+$comment = check_input($_POST["comment"], "Вы забыли написать сообщение!");
+/* Проверяем правильно ли записан e-mail */
+//if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $mail))
+//{
+//show_error("<br /> Е-mail адрес не существует");
+//}
+/* Создаем новую переменную, присвоив ей значение */
+$message_to_myemail = "Здравствуйте! 
+Вашей контактной формой было отправлено сообщение! 
+Имя отправителя: $name 
+E-mail: $mail 
+Текст сообщения: $comment
+Конец";
+/* Отправляем сообщение, используя mail() функцию */
+$from  = "From: $name <$mail> \r\n Reply-To: $mail \r\n"; 
+mail($myemail, $comment_to_myemail, $from);
+?>
+<p>Ваше сообщение было успешно отправлено!</p>
+<p>На <a href="index.html">Главную >>></a></p>
+<?php
+/* Если при заполнении формы были допущены ошибки сработает 
+следующий код: */
+function check_input($data, $problem = "")
+{
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+if ($problem && strlen($data) == 0)
+{
+show_error($problem);
 }
-$message = "<table style='width: 100%;'>$message</table>";
-function adopt($text) {
-	return '=?UTF-8?B?'.Base64_encode($text).'?=';
+return $data;
 }
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($name).'<vladi4ek1990@mail.ru>' . PHP_EOL .
-'Reply-To: '.$mail.'vladi4ek1990@mail.ru' . PHP_EOL;
-mail($mail, adopt($comment), $message, $headers );
+function show_error($myError)
+{
+?>
+<html>
+<body>
+<p>Пожалуйста исправьте следующую ошибку:</p>
+<?php echo $myError; ?>
+</body>
+</html>
+<?php
+exit();
+}
+?>
--- js/contact-form.js
+++ js/contact-form.js
@@ -1,28 +1,3 @@
-$(document).ready(function() {
-
-	//E-mail Ajax Send
-	$("contact-form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "contact.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-});
-
-
-
-
-
 //var $ = jQuery.noConflict();
 //
 //$(document).ready(function($) {
